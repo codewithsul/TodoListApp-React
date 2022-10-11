06ddb2958d65f14e168903eeaffced48b1701a4a
Suleiman123 <suleiman4a@gmail.com>
2022-10-11 07:11:20 -0700

    react to do

---
+++ src/components/AddTodo/AddTodo.jsx
@@ -0,0 +1,125 @@
+import React from "react";
+import { useRef } from "react";
+import { useEffect } from "react";
+import { useState } from "react";
+import "./AddTodo.css";
+
+const LOCAL_STORAGE_KEY = "Gleem";
+
+const AddTodo = () => {
+  const [AddToList, setList] = useState("");
+  const [TodoList, setTodoList] = useState([]);
+  const [completedList, setCompletedList] = useState([]);
+
+  const TodoNameRef = useRef();
+
+  const handleChange = (event) => {
+    const name = TodoNameRef.current.value;
+    if (name === "") return;
+    setList(name);
+  };
+
+  const AddList = () => {
+    const task = {
+      id: TodoList.length === 0 ? 1 : TodoList[TodoList.length - 1].id + 1,
+      taskName: AddToList,
+      completed: false,
+    };
+    const NewList = [...TodoList, task];
+    setTodoList(NewList);
+    TodoNameRef.current.value = null;
+  };
+
+  const handleDelete = (id) => {
+    const NewListDlt = TodoList.filter((task) => {
+      if (task.id === id) {
+        return false;
+      } else {
+        return true;
+      }
+    });
+    setTodoList(NewListDlt);
+  };
+
+  const handleAdd = (id) => {
+    setTodoList(
+      TodoList.map((task) => {
+        if (task.id === id) {
+          return { ...task, completed: true };
+        } else {
+          return { ...task, completed: false };
+        }
+      })
+    );
+    setCompletedList(TodoList);
+  };
+
+  useEffect(() => {
+    const TodoData = JSON.parse(localStorage.getItem(LOCAL_STORAGE_KEY));
+    if (TodoData) setTodoList(TodoData);
+  }, []);
+
+  useEffect(() => {
+    localStorage.setItem(LOCAL_STORAGE_KEY, JSON.stringify(TodoList));
+  }, [TodoList]);
+
+  return (
+    <>
+      <div className="leftblock">
+        <input
+          type="text"
+          placeholder="Add Todo"
+          name="TodoEntry"
+          id="input"
+          onChange={handleChange}
+          ref={TodoNameRef}
+        ></input>
+        <button type="submit" id="btn" onClick={AddList}>
+          Add
+        </button>
+        <hr className="horizontal"></hr>
+        <div className="TodoList">
+          {TodoList.map((task) => {
+            return (
+              <div className="Todos">
+                <h5>{task.taskName}</h5>
+                <button className="dlt" onClick={() => handleDelete(task.id)}>
+                  x
+                </button>
+                <button
+                  className="completed"
+                  onClick={() => handleAdd(task.id)}
+                >
+                  Add to Completed
+                </button>
+              </div>
+            );
+          })}
+        </div>
+      </div>
+      <div className="rightblock">
+        <div className="head">
+          <span className="dot"></span>
+          <h5>Completed</h5>
+        </div>
+        <hr className="horizontal2"></hr>
+        <div className="inner">
+          {completedList.map((task) => {
+            return (
+              <div
+                className="done"
+                style={{
+                  backgroundColor: "palegreen",
+                }}
+              >
+                <h5>{task.taskName}</h5>
+              </div>
+            );
+          })}
+        </div>
+      </div>
+    </>
+  );
+};
+
+export default AddTodo;
